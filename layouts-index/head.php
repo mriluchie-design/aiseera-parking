4f7685ec77441a8c46a32d27b5c1678b91f23dd1
iLugroup Multimedia <ilugroupdotcom@gmail.com>
2026-05-05 10:44:52 +0800

    .
    .

---
+++ layouts-index/head.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+
+<html lang="en">
+
+	<!-- begin::Head -->
+	<head>
+		<base href="">
+		<meta charset="utf-8" />
+		<title>AISEERA PARKING MANAGEMENT SYSTEM</title>
+		<meta name="description" content="Latest updates and statistic charts">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!--FAVICON-->
+		<link rel="shortcut icon" href="assets/img/logos/aiseera-parking-logo-2.png" />
+        <!--begin:: Vendor Plugins -->
+        <?php require'style.php' ?>
---
+++ layouts/head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+
+<html lang="en">
+
+	<!-- begin::Head -->
+	<head>
+		<base href="">
+		<meta charset="utf-8" />
+		<title>AISEERA PARKING MANAGEMENT SYSTEM</title>
+		<meta name="description" content="Latest updates and statistic charts">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="shortcut icon" href="../assets/img/logos/aiseera-parking-logo-2.png" />
+        <!--start::Page Scripts -->
+        <?php require'style.php' ?>
