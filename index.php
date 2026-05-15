f939ac37b9c28789a5fcba587e96c46833a8d067
iLugroup Multimedia <mriluchie@gmail.com>
2026-05-15 16:31:55 +0800

    Update index.php
    .

--- index.php
+++ index.php
@@ -109,7 +109,7 @@
 															
 															
 															
-															<a href="./laporan-operasional/index.php" class="kt-grid-nav__item">
+															<!--<a href="./laporan-operasional/index.php" class="kt-grid-nav__item">
 																<span class="kt-grid-nav__icon">
 																	<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" class="kt-svg-icon kt-svg-icon--danger kt-svg-icon--lg">
    <defs></defs>
@@ -120,7 +120,7 @@
     </g>
 </svg> </span>
 																<span class="kt-grid-nav__title">Laporan & Grafik</span>
-															</a>
+															</a>-->
 															<a href="./dashboard-operasional/dashboard.php" class="kt-grid-nav__item">
 																<span class="kt-grid-nav__icon">
 																	<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" class="kt-svg-icon kt-svg-icon--danger kt-svg-icon--lg">
