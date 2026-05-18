c87f7fa2f9f25cda9c07d199a8c0f36b190e7713
iLugroup Multimedia <mriluchie@gmail.com>
2026-05-19 02:04:06 +0800

    .
    .

--- layouts-index/header.php
+++ layouts-index/header.php
@@ -12,7 +12,7 @@
   <div id="kt_header_mobile" class="kt-header-mobile  kt-header-mobile--fixed ">
     <div class="kt-header-mobile__logo">
       <a href="index.php">
-        <img alt="Logo" src="assets/img/logos/aiseera-parking-logo-2.png" style="width:10%"/>
+        <img alt="Logo" src="assets/img/logos/epass-logo-landscape.png" style="width:10%"/>
       </a>
     </div>
     <div class="kt-header-mobile__toolbar">
