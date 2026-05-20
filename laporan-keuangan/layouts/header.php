3ca21d89da86525c8c4185773d3290713b62fc97
iLugroup Multimedia <mriluchie@gmail.com>
2026-05-20 11:40:38 +0800

    .
    .

--- laporan-keuangan/layouts/header.php
+++ laporan-keuangan/layouts/header.php
@@ -12,7 +12,7 @@
   <div id="kt_header_mobile" class="kt-header-mobile  kt-header-mobile--fixed ">
     <div class="kt-header-mobile__logo">
       <a href="#">
-        <img alt="Logo" src="../assets/img/logos/aiseera-parking-logo-2.png" style="width:80%"/>
+        <img alt="Logo" src="../assets/img/logos/epass-logo-only.png" style="width:80%"/>
       </a>
     </div>
     <div class="kt-header-mobile__toolbar">
