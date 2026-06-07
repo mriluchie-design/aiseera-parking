1565426fb6aa55575e96c8cb7c50ffab7c6f3719
iLugroup Multimedia <mriluchie@gmail.com>
2026-06-07 16:18:28 +0800

    .
    .

---
+++ assets/js/chart/cabang-monitoring-pendapatan-harian.js
@@ -0,0 +1,78 @@
+// Data retrieved from:
+// - https://en.as.com/soccer/which-teams-have-won-the-premier-league-the-most-times-n/
+// - https://www.statista.com/statistics/383679/fa-cup-wins-by-team/
+// - https://www.uefa.com/uefachampionsleague/history/winners/
+
+Highcharts.chart('container-cabang-monitoring-pendapatan-harian', {
+    chart: {
+        type: 'column',
+        marginTop: 70,
+        
+    },
+    title: {
+        text: '',
+        align: ''
+    },
+    xAxis: {
+        categories: ['Mobil Masuk', 'Motor Masuk', 'Mobil Keluar', 'Motor Keluar', 'Member', 'Kendaraan Berangkat', 'Muatan Kapal', 'Pass Kade', 'Denda']
+            
+    },
+    
+    yAxis: {
+        min: 0,
+        labels: {
+        formatter: function () {
+            // Menggunakan Highcharts.numberFormat untuk membulatkan
+            return Highcharts.numberFormat(this.value, 0, ',', '.'); 
+            // 0 di sini berarti 0 desimal
+        }
+        },
+        title: {
+            text: 'Rupiah'
+        },
+        stackLabels: {
+            enabled: true
+        }
+    },
+    legend: {
+        align: 'center',
+        x: 0,
+        verticalAlign: 'top',
+        y: 0,
+        floating: true,
+        backgroundColor: 'var(--highcharts-background-color, #ffffff)',
+        borderColor: 'var(--highcharts-neutral-color-20, #cccccc)',
+        borderWidth: 1,
+        valuePrefix: 'Rp. ',
+        valueDecimals: 0, // Jumlah angka di belakang koma
+        valueSuffix: ',-', // Tambahan opsional (contoh: ,-)
+        shadow: false
+    },
+    tooltip: {
+        headerFormat: '<b>{category}</b><br/>',
+        pointFormat: '{series.name}: {point.y}',
+        valuePrefix: 'Rp. ',
+        valueDecimals: 0, // Jumlah angka di belakang koma
+        valueSuffix: ',-' // Tambahan opsional (contoh: ,-)
+    },
+    plotOptions: {
+        column: {
+            stacking: 'normal',
+            dataLabels: {
+                enabled: false
+            }
+        }
+    },
+    
+    series: [{
+        name: 'e-Money',
+        data: [1500000000, 1000000000, 300000000, 300000000, 200000000, 150000000, 300000000, 186119460, 200000000,],
+        
+    }, {
+        name: 'QRIS',
+        data: [73090473, 152720540, 55301419, 111151352, 103125743, 160832635, 104610945, 100000000, 197232568,]
+    }]
+});
+
+
+
