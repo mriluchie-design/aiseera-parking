3c5e80fa9a42614d130f17d23b0daae89e1fcc16
iLugroup Multimedia <mriluchie@gmail.com>
2026-06-09 12:22:56 +0800

    .
    .

--- live-monitoring/cetak_monitoring_pendapatan_harian.php
+++ live-monitoring/cetak_monitoring_pendapatan_harian.php
@@ -1,136 +1,97 @@
+<!--
+Template Name: Metronic - Responsive Admin Dashboard Template build with Twitter Bootstrap 4 & Angular 8
+Author: KeenThemes
+Website: http://www.keenthemes.com/
+Contact: support@keenthemes.com
+Follow: www.twitter.com/keenthemes
+Dribbble: www.dribbble.com/keenthemes
+Like: www.facebook.com/keenthemes
+Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
+Renew Support: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
+License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
+-->
+
 <?php require '../layouts/head.php' ?>
-<link href="../assets/css/pages/wizard/wizard-3.css" rel="stylesheet" type="text/css" />
-<?php require './layouts/header.php' ?>
+<link href="../assets/dist/main/main.css" rel="stylesheet" type="text/css" />
+<?php require 'layouts/header.php' ?>
 
-<div class="kt-subheader   kt-grid__item" id="kt_subheader">
-        <div class="kt-container  kt-container--fluid ">
-            <div class="kt-subheader__main">
-                <h3 class="kt-subheader__title">
-                    <span class="kt-portlet__head-icon"><i class="kt-font-brand flaticon2-list-1"></i></span>&nbsp;&nbsp;
-                   Dashboard Cabang 
-                </h3>
-            <span class="kt-subheader__separator kt-subheader__separator--v"></span>
-			<div class="kt-subheader__group" id="kt_subheader_search">
-				<span class="kt-subheader__desc" id="kt_subheader_total" style="color:#ff0000">
-					Pendapatan</span>
-				<form class="kt-margin-l-20" id="kt_subheader_search_form">
-				</form>
+<div class="kt-subheader  kt-grid__item" id="kt_subheader">
+							<div class="kt-container  kt-container--fluid ">
+								<div class="kt-subheader__main">
+									<h3 class="kt-subheader__title">
+										<span class="kt-portlet__head-icon">
+											<i class="kt-font-brand flaticon2-list-1"></i>
+										</span>
+										&nbsp;
+										&nbsp;
+											Data Pendapatan
+									</h3>
+								<span class="kt-subheader__separator kt-subheader__separator--v"></span>
+									<div class="kt-subheader__group" id="kt_subheader_search">
+										<span class="kt-subheader__desc" id="kt_subheader_total" style="color:#ff0000">
+											Cabang Makassar </span>
+										<form class="kt-margin-l-20" id="kt_subheader_search_form">
+
+										</form>
+									</div>
+
+								</div>
+								<div class="kt-subheader__toolbar">
+								<a href="#.php" class="btn btn-label-brand btn-sm" data-toggle="dropdown" onclick="goBack()">
+												<i class="flaticon-reply"></i> Kembali</button>
+<script>
+function goBack() {
+  window.history.back();
+}
+</script>
+												<!--<i class="flaticon2-plus"></i>-->
+											</a>
+											
+											
+											
+			<a href="#" class="btn btn-label-success btn-sm" data-toggle="dropdown">
+				<i class="fa fa-print"></i> Export Data</button>
+			</a>
+			<div class="dropdown-menu dropdown-menu-fit dropdown-menu-md dropdown-menu-right">
+				<!--begin::Nav-->
+				<ul class="kt-nav">
+					<li class="kt-nav__item">
+						<a href="#" class="kt-nav__link">
+							<i class="kt-nav__link-icon fa fa-file-pdf"></i>
+							<span class="kt-nav__link-text">PDF</span>
+						</a>
+					</li>
+					<li class="kt-nav__item">
+						<a class="kt-nav__link" onclick="window.print();">
+							<i class="kt-nav__link-icon fa fa-print"></i>
+							<span class="kt-nav__link-text">Cetak</span>
+						</a>
+					</li>
+				</ul>
+				<!--end::Nav-->
 			</div>
 		</div>
-            <div class="kt-subheader__toolbar">
-                <button type="button" class="btn btn-label-danger btn-sm" id = "buttonLogin" onclick = "displayLoginBox()"><i class="flaticon-interface-6"></i> Filter Data</button>
-				
-                <a href="#" class="btn btn-label-success btn-sm" data-toggle="dropdown">
-                    <i class="flaticon2-printer"></i> Export Data</button>
-                    <!--<i class="flaticon2-plus"></i>-->
-                </a>
-                <div class="dropdown-menu dropdown-menu-fit dropdown-menu-md dropdown-menu-right">
-                    <!--begin::Nav-->
-                    <ul class="kt-nav">
-                        <li class="kt-nav__item">
-                            <a href="cetak_monitoring_pendapatan_harian.php" class="kt-nav__link">
-                                <i class="kt-nav__link-icon fa fa-file-pdf"></i><span class="kt-nav__link-text">PDF</span>
-                            </a>
-                          </li>
-                        <li class="kt-nav__item">
-                            <a href="#" class="kt-nav__link">
-                                <i class="kt-nav__link-icon fa fa-file-excel"></i><span class="kt-nav__link-text">Excel</span>
-                            </a>
-                        </li>
-                    </ul>
-                    <!--end::Nav-->
-                </div>
-				<div class="btn-group">
-				<button type="button" class="btn btn-label-google btn-sm">
-					<i class="fa fa-book-reader"></i>
-					<span class="kt-hidden-mobile">Bantuan</span>
-				</button>
-				
-				
-			</div>
-              
-            </div>
-			
-        </div>
-        </div>
-						
 
-            <div class="kt-container  kt-container--fluid  kt-grid__item kt-grid__item--fluid">
-			<div class="kt-portlet" id = "login_Box_Div" >
-            <div class="kt-portlet__head">
-                <div class="kt-portlet__head-label">
-                    <h3 class="kt-portlet__head-title">
-                        Filter Data
-                    </h3>
-                </div>
-            </div>
-            <!--begin::Form-->
-            <form class="kt-form">
-                <div class="kt-portlet__body">
-                    <div class="form-group row">
-					<div class="col-lg-2">
-                                <label>Cabang :</label>
-								
-                                <select class="form-control kt-selectpicker" data-live-search="true">
-                                      <option value="1" selected="">Makassar</option>
-                                      <option value="2">Pare-Pare</option>
-                                      <option value="2">Balikpapan</option>
-                                      <option value="2">Samarinda</option>
-                                      <option value="2">Tarakan</option>
-                                      <option value="2">Nunukan</option>
-                                      <option value="2">Manado</option>
-                                      <option value="2">Bitung</option>
-                                      <option value="2">Ambon - Yos Sudarso</option>
-                                      <option value="2">Ambon - Slamet Riyadi</option>
-                                      <option value="2">Ternate</option>
-                                      <option value="2">Sorong</option>
-                                      <option value="2">Keseluruhan</option>
-                                                                           
-                                  </select>
-                            </div>
-                      <div class="col-lg-2">
-                                <label class="">Periode :</label>
-                                <div class="input-group">
-						                        <input type="text" class="form-control form-control-sm kt_daterangepicker_2" readonly="" placeholder="-- Pilih Tanggal --">
-						                        <div class="input-group-append">
-						                          <span class="input-group-text"><i class="la la-calendar"></i></span>
-						                        </div>
-						                      </div>
-                            </div>  
-                        
-                        
-                       
-                    </div>
-                    <div class="kt-portlet__foot text-center">
-                        <div class="kt-form__actions">
-                            <button type="reset" class="btn btn-outline-brand btn-sm">Reset</button>
-                            <button type="reset" class="btn btn-primary btn-sm"><i class="flaticon-interface-6"></i>Filter Data</button>
-                        </div>
-                    </div>
-                </div>
-            </form>
-            <!--end::Form-->
-        
-		</div>
-		<div class="row">
-								
-								
-								
-								
-								
-								<div class="col-xl-12">
+							</div>
+						</div>
+
+							<div class="tab-content" style="background:#f2f3f8;overflow:scroll; padding-bottom">
+							<div class="kt-container  kt-container--fluid  kt-grid__item kt-grid__item--fluid mt-3">
+								<div class="bg-white w-70 ml-auto mr-auto">
+	<div>&nbsp;</div>
+<div class="col-xl-12">
 
 									<!--begin:: Widgets/Activity-->
 									<div class="kt-portlet kt-portlet--fit kt-portlet--head-lg kt-portlet--head-overlay kt-portlet--skin-solid kt-portlet--height-fluid">
 										<div class="kt-portlet__head kt-portlet__head--noborder kt-portlet__space-x">
 											<div class="kt-portlet__head-label">
 												<h3 class="kt-portlet__head-title">
-													Pendapatan
+													Pendapatan - Cabang Makassar
 												</h3>
 											</div>
 											<div class="kt-portlet__head-toolbar">
 												<a href="#" class="btn btn-label-light btn-sm " >
-													Hari ini
+													Periode : 01/01/2026 s/d 31/01/2026
 												</a>
 												
 											</div>
@@ -232,17 +193,9 @@
 
 									<!--end:: Widgets/Activity-->
 								</div>
-
-							<!--End::Section-->
-
-							
-
-						
-						</div>
-							
-								
-								<div class="row">
-						<div class="col-lg-6">
+<table class="table-checkable" cellpadding="3" align="center" width="1450" style="line-height:1.5; margin-left:63px; position:relative;top:0px; font-family: Tahoma; font-size: 12px;">
+  <tr>
+    <td width="50%"><div class="col-lg-12">
 							<div class="kt-portlet kt-portlet--height-fluid">
 										<div class="kt-portlet__head">
 											<div class="kt-portlet__head-label">
@@ -252,39 +205,17 @@
 				<form class="kt-margin-l-20" id="kt_subheader_search_form">
 												</h3>
 											</div>
-											<div class="kt-portlet__head-toolbar">
-												<a href="#" class="btn btn-label-brand btn-sm dropdown-toggle" data-toggle="dropdown">
-													Refresh
-												</a>
-												<div class="dropdown-menu dropdown-menu-fit dropdown-menu-right">
-													<ul class="kt-nav">
-														
-														
-														<li class="kt-nav__item">
-															<a href="#" class="kt-nav__link">
-																<i class="kt-nav__link-icon fa fa-sync-alt"></i>
-																<span class="kt-nav__link-text">Refresh Data</span>
-															</a>
-															
-														</li>
-														
-														
-														
-														
-													</ul>
-												</div>
-												
-										</div>
+											
 										</div>
 										<div class="kt-portlet__body"><br>
 											<figure class="highcharts-figure">
 											<figure class="highcharts-figure">
-													<div id="container-cabang-monitoring-pendapatan-harian" style="height:400px"></div>
+													<div id="container-cabang-monitoring-pendapatan-harian" style="height:300px"></div>
 											</figure>
 										</div>
 									</div>
-						</div>
-						<div class="col-lg-6">
+						</div></td>
+    <td width="50%"><div class="col-lg-12">
 							<div class="kt-portlet kt-portlet--height-fluid">
 										<div class="kt-portlet__head">
 											<div class="kt-portlet__head-label">
@@ -293,45 +224,24 @@
 					By Provider</span>
 												</h3>
 											</div>
-											<div class="kt-portlet__head-toolbar">
-												<a href="#" class="btn btn-label-brand btn-sm dropdown-toggle" data-toggle="dropdown">
-													Refresh
-												</a>
-												<div class="dropdown-menu dropdown-menu-fit dropdown-menu-right">
-													<ul class="kt-nav">
-														
-														
-														<li class="kt-nav__item">
-															<a href="#" class="kt-nav__link">
-																<i class="kt-nav__link-icon fa fa-sync-alt"></i>
-																<span class="kt-nav__link-text">Refresh Data</span>
-															</a>
-															
-														</li>
-														
-														
-														
-														
-													</ul>
-												</div>
-												
-										</div>
+											
 										</div>
 										<div class="kt-portlet__body">
 											<figure class="highcharts-figure">
-										    <div id="container-cabang-monitoring-transaksi-provider" style="height:400px"></div>
+										    <div id="container-cabang-monitoring-transaksi-provider" style="height:332px"></div>
 										</figure>
 										</div>
 									</div>
-						</div>
-						
-						
-						
-						
-					</div>
-					<div class="row">
-						
-						<div class="col-lg-6">
+						</div></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+<table class="table-checkable" cellpadding="3" align="center" width="1450" style="line-height:1.5; margin-left:63px; position:relative;top:0px; font-family: Tahoma; font-size: 12px;">
+  <tr>
+    <td width="50%"><div class="col-lg-12">
 							<div class="kt-portlet kt-portlet--height-fluid">
 										<div class="kt-portlet__head">
 											<div class="kt-portlet__head-label">
@@ -340,65 +250,78 @@
 					Member </span>
 												</h3>
 											</div>
-											<div class="kt-portlet__head-toolbar">
-												<a href="#" class="btn btn-label-brand btn-sm dropdown-toggle" data-toggle="dropdown">
-													Refresh
-												</a>
-												<div class="dropdown-menu dropdown-menu-fit dropdown-menu-right">
-													<ul class="kt-nav">
-														
-														
-														<li class="kt-nav__item">
-															<a href="#" class="kt-nav__link">
-																<i class="kt-nav__link-icon fa fa-sync-alt"></i>
-																<span class="kt-nav__link-text">Refresh Data</span>
-															</a>
-															
-														</li>
-														
-														
-														
-														
-													</ul>
-												</div>
-												
-										</div>
+											
 										</div>
 										<div class="kt-portlet__body">
 											<figure class="highcharts-figure">
-										    <div id="container-detail-pendapatan-member" style="height:400px"></div>
+										    <div id="container-detail-pendapatan-member" style="height:330px"></div>
 										</figure>
 										</div>
 									</div>
+						</div></td>
+    <td width="50%"></td>
+  </tr>
+  
+</table>						
+<table align="center" class="footnote-1" width="1450px" style="margin-top:50px; margin-bottom:20px;">
+        <thead>
+            <tr><th></th>
+            <th width="2%"></th>
+            <th></th>
+            <th></th>
+        </tr></thead>
+        <tbody>
+            <tr>
+                <td colspan="4" style="border-bottom: 1px solid #ccc;"></td>
+            </tr>
+            
+            <tr style="height: 10px"><td></td></tr>
+            <tr style=" width:650px;">
+                <td colspan="2" rowspan="2" style="text-align: center;">
+                    <span class="span-qr" style="z-index: 99999">
+                        <!--?xml version="1.0" encoding="UTF-8"?-->
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="40" height="40" viewBox="0 0 40 40"><rect x="0" y="0" width="40" height="40" fill="#ffffff"></rect><g transform="scale(1.905)"><g transform="translate(0,0)"><path fill-rule="evenodd" d="M9 0L9 1L8 1L8 3L9 3L9 4L8 4L8 8L6 8L6 9L5 9L5 10L4 10L4 8L3 8L3 9L2 9L2 8L0 8L0 9L2 9L2 10L3 10L3 12L4 12L4 11L7 11L7 10L8 10L8 8L9 8L9 11L8 11L8 12L6 12L6 13L8 13L8 15L9 15L9 16L10 16L10 17L11 17L11 16L12 16L12 17L13 17L13 18L12 18L12 19L10 19L10 20L11 20L11 21L12 21L12 20L13 20L13 19L14 19L14 21L15 21L15 18L16 18L16 19L17 19L17 20L19 20L19 18L20 18L20 20L21 20L21 17L20 17L20 16L21 16L21 14L20 14L20 15L19 15L19 16L18 16L18 13L19 13L19 12L20 12L20 13L21 13L21 12L20 12L20 11L19 11L19 10L21 10L21 9L20 9L20 8L18 8L18 9L17 9L17 8L14 8L14 9L13 9L13 8L12 8L12 7L13 7L13 6L12 6L12 7L11 7L11 6L10 6L10 7L9 7L9 4L10 4L10 0ZM12 0L12 1L11 1L11 4L12 4L12 5L13 5L13 2L12 2L12 1L13 1L13 0ZM6 9L6 10L7 10L7 9ZM11 9L11 10L10 10L10 11L9 11L9 12L8 12L8 13L9 13L9 15L10 15L10 16L11 16L11 14L13 14L13 15L12 15L12 16L13 16L13 17L17 17L17 19L18 19L18 18L19 18L19 17L18 17L18 16L17 16L17 15L16 15L16 14L13 14L13 13L16 13L16 12L14 12L14 10L15 10L15 11L17 11L17 13L18 13L18 12L19 12L19 11L18 11L18 10L19 10L19 9L18 9L18 10L15 10L15 9L14 9L14 10L13 10L13 9ZM12 10L12 11L11 11L11 12L12 12L12 13L13 13L13 12L12 12L12 11L13 11L13 10ZM1 11L1 12L0 12L0 13L1 13L1 12L2 12L2 11ZM9 12L9 13L10 13L10 12ZM14 15L14 16L16 16L16 15ZM8 17L8 18L9 18L9 17ZM8 19L8 21L9 21L9 19ZM0 0L0 7L7 7L7 0ZM1 1L1 6L6 6L6 1ZM2 2L2 5L5 5L5 2ZM14 0L14 7L21 7L21 0ZM15 1L15 6L20 6L20 1ZM16 2L16 5L19 5L19 2ZM0 14L0 21L7 21L7 14ZM1 15L1 20L6 20L6 15ZM2 16L2 19L5 19L5 16Z" fill="#000000"></path></g></g></svg>
+
+                    </span>
+                </td>
+                <td colspan="2" valign="left" style="text-align: center">
+                    <span style="text-align:justify; line-height:1.5; font-family: Roboto; font-size: 11px; "><i>Sesuai
+                            dengan ketentuan Peraturan Perusahaan yang berlaku, Perusahaan mengatur
+                            bahwa
+                            Dokumen ini telah ditandatangani dan diverifikasi secara elektronik, sehingga
+                            tidak
+                            diperlukan
+                            tanda tangan basah pada Dokumen ini.</i></span></td>
+            </tr>
+            <tr style=" width:650px;">
+              <td colspan="2" valign="center" style="text-align: center; font-family: Roboto; font-size: 11px;"><i>Document Printed By : Muhammad Nasrullah, S.Kom - 16/11/2025 - 20:39 WITA - Halaman 1 dari 100</i></td>
+            </tr>
+<tr style="height: 10px"><td></td></tr>
+             <tr>
+			 
+                <td colspan="4" style="border-bottom: 1px solid #ccc; padding-bottom"></td>
+            </tr>
+			<tr>
+			 
+                <td>&nbsp;</td>
+            </tr>
+			<tr>
+			 
+                <td>&nbsp;</td>
+            </tr>
+        </tbody>
+    </table>
+  
+  
+								</div>
+
+								<!--End:: App Content-->
+							</div>
+
+							<!--End::App-->
 						</div>
-						
-						
-						
-						
-					</div>
-					
-					<div>&nbsp;</div>
-					<div>&nbsp;</div>
-					
 
-								
-									
-									
-									</div>
-									
-									
-								</div>
-								</div>
-								
-								
-									
-									
-								</div>
-
-								
-						</div>
-
-						<?php require '../layouts/footer.php' ?>
+<?php require '../layouts/footer.php' ?>
 						<script src="https://code.highcharts.com/highcharts.js"></script>
 						<script src="https://code.highcharts.com/modules/exporting.js"></script>
 						<script src="https://code.highcharts.com/modules/export-data.js"></script>
