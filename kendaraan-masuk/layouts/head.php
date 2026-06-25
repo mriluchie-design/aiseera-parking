f3dcd14b349d04a73b203bad7ae86677c012cd84
iLugroup Multimedia <mriluchie@gmail.com>
2026-06-25 21:36:58 +0800

    Update head.php
    .

--- kendaraan-masuk/layouts/head.php
+++ kendaraan-masuk/layouts/head.php
@@ -20,7 +20,7 @@ License: You must have a valid license purchased only from themeforest(the above
 		<meta charset="utf-8" />
 		<title>CUSTOMER PORTAL</title>
 		<meta name="description" content="Latest updates and statistic charts">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="shortcut icon" href="../assets/img/logos/epass-logo-only.png" />
         <!--start::Page Scripts -->
         <?php require'layouts/style.php' ?>
