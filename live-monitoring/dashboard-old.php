62bebddbe6e697fd11f822a2af0fc45c6ad791e7
Aksi Kode Indonesia <mriluchie@gmail.com>
2026-07-21 00:10:33 +0800

    .
    .

--- live-monitoring/dashboard-old.php
+++ live-monitoring/dashboard-old.php
@@ -1,10 +1,10 @@
 <?php require '../layouts/head.php' ?>
-<?php require 'layouts/header.php' ?>
+<link href="../assets/css/pages/wizard/wizard-3.css" rel="stylesheet" type="text/css" />
+<?php require './layouts/header.php' ?>
 
-    <!-- begin:: Content Head -->
-    <div class="kt-subheader   kt-grid__item" id="kt_subheader">
-        <div class="kt-container  kt-container--fluid ">
-            <div class="kt-subheader__main">
+<div class="kt-subheader  kt-grid__item" id="kt_subheader">
+							<div class="kt-container  kt-container--fluid ">
+								<div class="kt-subheader__main">
 									<h3 class="kt-subheader__title">
 										<span class="kt-portlet__head-icon">
 											<i class="kt-font-brand flaticon2-list-1"></i>
@@ -24,9 +24,9 @@
 									</div>
 
 								</div>
-            <div class="kt-subheader__toolbar">
-			
+								<div class="kt-subheader__toolbar">
                 <button type="button" class="btn btn-label-danger btn-sm" id = "buttonLogin" onclick = "displayLoginBox()"><i class="flaticon-interface-6"></i> Filter Data</button>
+				
                 <a href="#" class="btn btn-label-success btn-sm" data-toggle="dropdown">
                     <i class="flaticon2-printer"></i> Export Data</button>
                     <!--<i class="flaticon2-plus"></i>-->
@@ -55,17 +55,13 @@
 				
 				
 			</div>
-              
-            </div>
-			
-        </div>
-    </div>
-	<!-- end:: Content Head -->
+							</div>
+						</div>
+						</div>
+						
 
-	<!-- begin:: Content -->
-	<div class="kt-container  kt-container--fluid  kt-grid__item kt-grid__item--fluid">
-
-        <div class="kt-portlet" id = "login_Box_Div" >
+            <div class="kt-container  kt-container--fluid  kt-grid__item kt-grid__item--fluid">
+			<div class="kt-portlet" id = "login_Box_Div" >
             <div class="kt-portlet__head">
                 <div class="kt-portlet__head-label">
                     <h3 class="kt-portlet__head-title">
@@ -75,10 +71,10 @@
             </div>
             <!--begin::Form-->
             <form class="kt-form">
-					<div class="kt-portlet__body">
-                        <div class="form-group row">
-						<div class="col-lg-2">
-                                <label>Lokasi :</label>
+                <div class="kt-portlet__body">
+                    <div class="form-group row">
+					<div class="col-lg-2">
+                                <label>Cabang :</label>
 								
                                 <select class="form-control kt-selectpicker" data-live-search="true">
                                       <option value="1" selected="">Makassar</option>
@@ -96,40 +92,38 @@
                                       <option value="2">Keseluruhan</option>
                                                                            
                                   </select>
-    		                    </div>
-                             
-							
-							
-							
-							
-    		                    </div>
-								
-                            
-							
-                            
-							
-							
-							
-							<br>
-							
-							
-						
-					<div class="kt-portlet__foot text-center">
-						<div class="kt-form__actions">
-							<button type="reset" class="btn btn-secondary btn-sm">Reset</button>
-							<button type="reset" class="btn btn-primary btn-sm"><i class="flaticon-interface-6"></i>Filter Data</button>
-						</div>
-					</div>
-					</div>
-				</form>
+                            </div>
+                        
+                        
+                        
+                       
+                    </div>
+                    <div class="kt-portlet__foot text-center">
+                        <div class="kt-form__actions">
+                            <button type="reset" class="btn btn-secondary btn-sm">Reset</button>
+                            <button type="reset" class="btn btn-primary btn-sm"><i class="flaticon-interface-6"></i>Filter Data</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
             <!--end::Form-->
         </div>
+			<div class="row">
+								
 
-        <!--Begin::Section-->
-								<div class="kt-portlet__body">
-		<div class="row">
+							<!--end:: Portlet-->
+									
 
-			<div class="col-xl-9">
+									<!--begin:: Widgets/Outbound Bandwidth-->
+									
+									
+
+									<!--end:: Widgets/Outbound Bandwidth-->
+							
+								
+								
+								
+								<div class="col-xl-9">
 
 									<!--begin:: Widgets/Activity-->
 									<div class="kt-portlet kt-portlet--fit kt-portlet--head-lg kt-portlet--head-overlay kt-portlet--skin-solid kt-portlet--height-fluid">
@@ -492,7 +486,20 @@
 									<!--end:: Widgets/Activity-->
 								</div>
 								
-<div class="col-xl-3">
+										
+
+							<!--End::Section-->
+
+							
+
+						
+						</div>
+			<div class="row">
+								
+								
+								
+								
+								<div class="col-xl-3">
 				<!--begin:: Portlet-->
 				<div class="kt-portlet kt-portlet--height-fluid kt-portlet__head kt-ribbon kt-ribbon--right kt-ribbon--success">
                         <!--begin::status -->
@@ -1004,19 +1011,36 @@
 					</div>
 				<!--end:: Portlet-->
 			</div>
+							
+								</div>
 			
-			</div>
-			
-			
-			
-		
-			
-		</div>
-		<div>&nbsp;</div>
-		
+							
+								
+						
+						
+						
+						
+						
+					</div>
+					<div>&nbsp;</div>
+					
 
-		<!--End::Section-->
-<div class="modal fade" id="modal_bantuan" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+								
+									
+									
+									</div>
+								</div>
+								</div>
+								
+								
+									
+									
+								</div>
+
+								
+						</div>
+<!--begin::Modal 1-->
+									<div class="modal fade" id="modal_bantuan" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
 								<div class="modal-dialog modal-dialog-centered modal-sm" role="document">
 									<div class="modal-content">
 
@@ -6786,19 +6810,50 @@
 
   </div>
   </div>
+  </div>
+  
   
 
 
 
 
-	
-
-<?php require '../layouts/footer.php' ?>
-
-
-<script src="../assets/dist/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js" type="text/javascript"></script>
-<script src="../assets/js/pages/crud/forms/widgets/bootstrap-datepicker.js" type="text/javascript"></script>
-<script src="../assets/dist/bootstrap-select/dist/js/bootstrap-select.js" type="text/javascript"></script>
-<script src="../assets/js/pages/crud/forms/widgets/bootstrap-select.js" type="text/javascript"></script>
-
-<?php require '../layouts/foot.php' ?>
+						<?php require '../layouts/footer.php' ?>
+						<script src="https://code.highcharts.com/highcharts.js"></script>
+						<script src="https://code.highcharts.com/modules/exporting.js"></script>
+						<script src="https://code.highcharts.com/modules/export-data.js"></script>
+						<script src="https://code.highcharts.com/modules/accessibility.js"></script>
+						<script src="../assets/js/chart/highchart.js" type="text/javascript"></script>
+						<script src="../assets/js/chart/highchart-kendaraan-masukvskeluar.js" type="text/javascript"></script>
+						<?php require '../layouts/foot.php' ?>
+						<?php require './layouts/script.php' ?>
+						<script>
+$(document).READY(function(){
+$(".btn-collapse-onboarding").click(function(){
+	$(".show-large-tb").hide(1000);
+});
+$("#btnShowHide").click(function(){
+	$("#divShowHide").toggle();
+});
+$("#btnShowHide2").click(function(){
+	$("#divShowHide2").toggle();
+});
+$("#btnShowHide3").click(function(){
+	$("#divShowHide3").toggle();
+});
+$("#btnShowHide4").click(function(){
+	$("#divShowHide4").toggle();
+});
+$("#btnShowHide5").click(function(){
+	$("#divShowHide5").toggle();
+});
+$("#btnShowHide6").click(function(){
+	$("#divShowHide6").toggle();
+});
+$("#btnShowHide7").click(function(){
+	$("#divShowHide7").toggle();
+});
+$("#btnShowHide8").click(function(){
+	$("#divShowHide8").toggle();
+});
+});
+</script>
